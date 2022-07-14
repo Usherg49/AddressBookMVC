1e514ffbce01a30c5a1a06cf0aa4807d9057d7a3
Usherg49 <78185241+Usherg49@users.noreply.github.com>
2022-07-14 17:24:00 -0400

    Heroku publishing - DB
    Wired up DB with connection strings and auto migration upon publish to Heroku

--- Startup.cs
+++ Startup.cs
@@ -22,13 +22,15 @@ namespace AddressBookMVC
             Configuration = configuration;
         }
 
+
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(DataUtility.GetConnectionString(Configuration)));
 
             services.AddScoped<IImageService, BasicImageService>();
 
